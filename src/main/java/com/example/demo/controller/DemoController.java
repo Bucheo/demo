f3742e7aa850a879c53f94d485a58e4b3715f62f
Bucheo <atfqwe80@gmail.com>
2025-09-24 14:31:09 +0900

    demo

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,12 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.model.domain.TestDB;
+import com.example.demo.model.service.TestService;
+
 @Controller // 컨트롤러 어노테이션 명시
 public class DemoController {
+    @Autowired
+    private TestService testService;
+    
     @GetMapping("/hello") // 전송 방식 GET
     public String hello(Model model) {
         model.addAttribute("data", " 반갑습니다."); // model 설정
@@ -27,5 +34,13 @@ public class DemoController {
         model.addAttribute("para1", "001");
         model.addAttribute("para2", 002);
         return "thymeleaf_test1";
+    }
+
+    @GetMapping("/testdb")
+    public String getAllTestDBs(Model model) {
+        TestDB test = testService.findByName("홍길동");
+        model.addAttribute("data4", test);
+        System.out.println("데이터 출력 디버그 : " + test);
+        return "testdb";
     }
 }
